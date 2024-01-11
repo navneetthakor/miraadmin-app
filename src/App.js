48392d38ed28375466d00c4c9e342afa567d18fc
navneetthakor <tnavneer8628@gmail.com>
2024-01-11 22:37:49 +0530

    router-setup + theme.js created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
+// router setup
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom"; 
+import RootLayout from "./scens/RootLayout.js";
+import Home from "./scens/Home.js";
+
 function App() {
+
+  // setting-up router 
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route exact path='/' element={<RootLayout/>}>
+      <Route index element={<Home/>}/>
+    </Route>
+  ))
   return (
-    <div className="App">
-      <h1>Hello, how are you?</h1>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
